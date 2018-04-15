327b101dd3edcaf3ff2865027c21436b2f56fcb1
Torres <torresnceuit@gmail.com>
2018-04-15 11:59:32 +0700

    2. Tranfer Token

--- contracts/DsToken.sol
+++ contracts/DsToken.sol
@@ -3,22 +3,43 @@ pragma solidity ^0.4.21;
 /**
  * The DToken contract does this and that...
  */
-contract DsToken {
-	/**
-	 * set the total number of tokens
-	 * get the total number of tokens
+ contract DsToken {
+	/*
+	 * Set the total number of tokens
+	 * Get the total number of tokens
 	 */
-	string public name = "Ds Token";
-	string public symbol = "DS";
-	string public standard = "Ds Token v1.0.0";
-	uint256 public totalSupply;
-	// map the address to the value of balance
-	mapping (address => uint256) public balanceOf;
-	
-	function DsToken (uint256 _initialSupply) public {
-		totalSupply = _initialSupply;
-		balanceOf[msg.sender] = _initialSupply;
-	}	
+	 string public name = "Ds Token";
+	 string public symbol = "DS";
+	 string public standard = "Ds Token v1.0.0";
+	 uint256 public totalSupply;
+	 //Map the address to the value of balance
+	 mapping (address => uint256) public balanceOf;
 
-	
-}
+	 function DsToken (uint256 _initialSupply) public {
+	 	totalSupply = _initialSupply;
+	 	balanceOf[msg.sender] = _initialSupply;
+	 }	
+
+	 event Transfer(
+	 	address indexed _from,
+	 	address indexed _to,
+	 	uint256 _value 
+	 );
+
+
+
+	 //Tranfer function
+	 function tranfer (address _to, uint256 _value) public returns(bool success){
+	 	//Exception if account doesn't have enough 
+	 	require(balanceOf[msg.sender] >= _value);
+	 	//Tranfer balance
+	 	balanceOf[msg.sender] -= _value;
+	 	balanceOf[_to] += _value;
+	 	//Trigger tranfer event
+	 	Transfer(msg.sender, _to, _value);
+	 	//Return a boolean
+	 	return true;
+
+	 }
+
+	}
--- test/DsToken.js
+++ test/DsToken.js
@@ -18,6 +18,7 @@ contract("DsToken",function (accounts) {
 			assert.equal(standard, "Ds Token v1.0.0", "it has correct standard")
 		})
 	})
+
 	it("set the total number of tokens",function(){
 		return DsToken.deployed().then(function(instance){
 			tokenInstance = instance;
@@ -29,4 +30,30 @@ contract("DsToken",function (accounts) {
 			assert.equal(adminBalance.toNumber(), 1000000, "it set the totalSupply of admin account to 1000000")
 		})
 	})
+
+	it("tranfer tokens of ownership",function(){
+		return DsToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			return tokenInstance.tranfer.call(accounts[1],999999999999)
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >=0, "error must have revert")
+			return tokenInstance.tranfer.call(accounts[1],250000,{from: accounts[0]})
+		}).then(function(success){
+			assert.equal(success, true, "it return true")
+			return tokenInstance.tranfer(accounts[1],250000,{from: accounts[0]})
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+			assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+			assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+			assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+			return tokenInstance.balanceOf(accounts[1])
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 250000, "adds the amount to the receiving account")
+			return tokenInstance.balanceOf(accounts[0])
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 750000, "deducts the amount from sending account")
+		})
+	})
+
 })
